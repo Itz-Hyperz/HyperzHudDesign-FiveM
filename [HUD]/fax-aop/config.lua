438e95fd819f448adf4000f8f401a9db8f4d5b52
Itz-Hyperz <69090660+Itz-Hyperz@users.noreply.github.com>
2020-11-16 10:21:12 -0500

    Add files via upload

---
+++ [HUD]/fax-aop/config.lua
@@ -0,0 +1,97 @@
+--[[
+    -----------------------------------
+    -- Area of Patrol, Made by FAXES --
+    --          CONFIG FILE          --
+    -----------------------------------
+
+    !!!!!!!!!IMPORTANT!!!!!!!!!
+    To see what each variable (configurable option) below does. Please view the link right below...
+
+    Default / Variable Docs: https://docs.faxes.zone/docs/aop-setup/#variable-documentation
+]]
+
+--[[
+    1. General Config & Commands
+--]]
+
+    FaxCurAOP = "None Set"
+    usingPerms = true
+    AOPChangeNotification = true
+
+    AOPCommand = "aop"
+    PTCommand = "pt"
+
+    featColor = "~c~"
+    noPermsMessage = "~r~Insufficient Permissions."
+
+--[[
+    2. Peacetime Settings
+--]]
+
+    peacetime = false
+    peacetimeNS = true
+    maxPTSpeed = 100
+
+    PTOnMessage = featColor .. "Peace Time ~w~is now ~g~ in effect!"
+    PTOffMessage = featColor .. "Peace Time ~w~is now ~r~off."
+
+
+--[[
+    3. DrawText Settings
+--]]
+
+    AOPLocation = 6
+    serverPLD = false
+    localTime = 1
+    
+    AOPx = 0.660
+    AOPy = 1.332
+
+
+--[[
+    4. Auto AOP Settings
+--]]
+
+    autoChangeAOP = false
+
+    ACAOPUnder5 = "Paleto Bay"
+    ACAOPUnder10 = "Sandy Shores"
+    ACAOPUnder20 = "Blaine County"
+    ACAOPUnder30 = "Los Santos"
+    ACAOPOver30 = "San Andreas"
+
+
+--[[
+    5. AOP Spawn Locations
+--]]
+
+    AOPSpawnsEnabled = false
+    AOPSpawns = {
+        {
+            AOPName = 'sandy shores',
+            AOPCoords  = {x = 311.22, y = 3457.60, z = 36.15}
+        },
+        {
+            AOPName = 'paleto bay',
+            AOPCoords  = {x = -447.24, y = 5970.46, z = 31.78}
+        },
+        {
+            AOPName = 'downtown',
+            AOPCoords  = {x = 219.98, y = -913.38, z = 30.69}
+        },
+        {
+            AOPName = 'rockford hills',
+            AOPCoords  = {x = -851.57, y = -128.04, z = 37.62}
+        },
+    }
+
+--[[
+    6. Discord Permissions
+    Make sure to have discordroles installed and configured!
+--]]
+
+    usingDiscordPerms = false
+    discordRoleIds = {
+        "Role_ID",
+        "Role_ID"
+    }
---
+++ [HUD]/fax-aop/server.lua
@@ -0,0 +1,161 @@
+-----------------------------------
+-- Area of Patrol, Made by FAXES --
+-----------------------------------
+
+--- NO NEED TO EDIT THIS FILE!!!! EDIT THE CONFIG.LUA ---
+--- NO NEED TO EDIT THIS FILE!!!! EDIT THE CONFIG.LUA ---
+--- NO NEED TO EDIT THIS FILE!!!! EDIT THE CONFIG.LUA ---
+--- NO NEED TO EDIT THIS FILE!!!! EDIT THE CONFIG.LUA ---
+--- NO NEED TO EDIT THIS FILE!!!! EDIT THE CONFIG.LUA ---
+
+FaxCurPT = false
+curVersion = "3.4"
+
+function GetDiscordPermissionSet(src)
+	if usingDiscordPerms then
+		for k, v in ipairs(GetPlayerIdentifiers(src)) do
+			if string.sub(v, 1, string.len("discord:")) == "discord:" then
+				identifierDiscord = v
+			end
+		end
+
+		if identifierDiscord then
+			exports['discordroles']:isRolePresent(src, discordRoleIds, function(hasRole, roles)
+				if not roles then
+					return false
+				end
+				if hasRole then
+					return true
+				else
+					return false
+				end
+			end)
+		else
+			print("^1[Fax-AOP] No Discord ID found for '" .. GetPlayerName(src) .. "'^7")
+			return false
+		end
+	else
+		return false
+	end
+end
+
+RegisterServerEvent('AOP:Startup')
+AddEventHandler('AOP:Startup', function()
+	Wait(3000)
+	TriggerClientEvent("AOP:RunConfig", -1)
+	Wait(30000)
+	SetMapName("RP : " .. FaxCurAOP)
+end)
+
+TriggerEvent("AOP:Startup")
+
+RegisterCommand(AOPCommand, function(source, args, rawCommand)
+	if source == 0 or IsPlayerAceAllowed(source, "faxes.aopcmds") or GetDiscordPermissionSet(source) or not usingPerms then
+		FaxCurAOP = table.concat(args, " ")
+		if(source == 0)then;print("AOP changed to: " .. FaxCurAOP);end
+		TriggerEvent("AOP:Sync")
+		SetMapName("RP : " .. FaxCurAOP)
+		if AOPChangeNotification then
+			TriggerClientEvent("AOP:DisNotification", -1, featColor .. "Area of Patrol ~w~has changed!~n~AOP: " .. FaxCurAOP)
+		end
+	else
+		TriggerClientEvent('AOP:NoPerms', source)
+	end
+end)
+
+
+RegisterServerEvent('AOP:Sync')
+AddEventHandler('AOP:Sync', function()
+	TriggerClientEvent('AOP:SendAOP', -1, FaxCurAOP)
+end)
+
+RegisterCommand(PTCommand, function(source, args, rawCommand)
+	if peacetime then
+		if source == 0 or IsPlayerAceAllowed(source, "faxes.aopcmds") or GetDiscordPermissionSet(source) or not usingPerms then
+			if(source == 0)then;print("Peacetime toggled");end
+			if not FaxCurPT then
+				TriggerClientEvent("AOP:DisNotification", -1, PTOnMessage)
+				FaxCurPT = true
+				TriggerEvent('AOP:PTSync')
+			elseif FaxCurPT then
+				TriggerClientEvent("AOP:DisNotification", -1, PTOffMessage)
+				FaxCurPT = false
+				TriggerEvent('AOP:PTSync')
+			end
+		else
+			TriggerClientEvent('AOP:NoPerms', source)
+		end
+	end
+end)
+
+RegisterServerEvent('AOP:PTSync')
+AddEventHandler('AOP:PTSync', function()
+	TriggerClientEvent('AOP:SendPT', -1, FaxCurPT)
+end)
+
+Citizen.CreateThread(function()
+    while autoChangeAOP do
+		local players = GetPlayers()
+		for a = 1, #players do
+			if players[a] > "30" then -- over 30
+				FaxCurAOP = ACAOPOver30
+				SetMapName("RP : " .. FaxCurAOP)
+				TriggerEvent("AOP:Sync")
+			elseif players[a] < "5" then -- under 5
+				FaxCurAOP = ACAOPUnder5
+				SetMapName("RP : " .. FaxCurAOP)
+				TriggerEvent("AOP:Sync")
+			elseif players[a] < "10" then -- under 10
+				FaxCurAOP = ACAOPUnder10
+				SetMapName("RP : " .. FaxCurAOP)
+				TriggerEvent("AOP:Sync")
+			elseif players[a] < "20" then -- under 20
+				FaxCurAOP = ACAOPUnder20
+				SetMapName("RP : " .. FaxCurAOP)
+				TriggerEvent("AOP:Sync")
+			elseif players[a] < "30" then -- under 30
+				FaxCurAOP = ACAOPUnder30
+				SetMapName("RP : " .. FaxCurAOP)
+				TriggerEvent("AOP:Sync")
+			end
+		end
+		TriggerEvent("AOP:Sync")
+		Citizen.Wait(120 * 1000)
+	end
+end)
+
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! --
+-----------------------------------------------------------
+-- THE BELOW IS FOR DEBUGGING AND CHECKERS. DO NOT TOUCH --
+--       Touching the below results in NO support!       --
+-----------------------------------------------------------
+
+RegisterCommand("aopstatus", function(source, args, rawCommand)
+	TriggerClientEvent("Fax:ClientPrint", source, "Current AOP: " .. FaxCurAOP)
+	TriggerClientEvent("Fax:ClientPrint", source, "AOP Command: " .. AOPCommand)
+	TriggerClientEvent("Fax:ClientPrint", source, "PT Command: " .. PTCommand)
+	TriggerClientEvent("Fax:ClientPrint", source, "Feat Color: " .. featColor)
+	TriggerClientEvent("Fax:ClientPrint", source, "Auto Change AOP: " .. tostring(autoChangeAOP))
+	TriggerClientEvent("Fax:ClientPrint", source, "Using Permissions: " .. tostring(usingPerms))
+	TriggerClientEvent("Fax:ClientPrint", source, "Peacetime Func Enabled: " .. tostring(peacetime))
+	TriggerClientEvent("Fax:ClientPrint", source, "AOP Location Setting: " .. tostring(AOPLocation))
+	TriggerClientEvent("Fax:ClientPrint", source, "AOP Spawn Points: " .. tostring(AOPSpawnsEnabled))
+	if AOPLocation == 6 then
+		TriggerClientEvent("Fax:ClientPrint", source, "AOP Location X Setting: " .. tostring(AOPx))
+		TriggerClientEvent("Fax:ClientPrint", source, "AOP Location Y Setting: " .. tostring(AOPy))
+	end
+	TriggerClientEvent("Fax:ClientPrint", source, "------------------")
+	TriggerClientEvent("Fax:ClientPrint", source, "Current Version: " .. curVersion)
+	TriggerClientEvent("Fax:ClientPrint", source, "Script Credits: Script made by FAXES, Discord: FAXES#8655 - http://faxes.zone/discord")
+end)
+
+PerformHttpRequest("https://raw.githubusercontent.com/FAXES/ResourceStats/master/AOP.json", function(err, shit, headers)
+	local data = json.decode(shit)
+	if data.announce == 1 and curVersion < data.version then
+		print("\n^5[Fax-AOP ^7- ^3Notice^5]^5 New Script Version: ^7" .. data.version .. ". ^5New Announcement: ^7" .. data.message .. "\n")
+	elseif data.announce == 1 then
+		print("\n^5[Fax-AOP ^7- ^1Announcement^5]^7 " .. data.message .. "\n")
+	elseif curVersion < data.version then
+		print("\n^5[Fax-AOP ^7- ^3Notice^5]^7 Fax-AOP has a new version! Your version: " .. curVersion .. ". New version: " .. data.version .. "\nChangelog: " .. data.changelog .. "\n")
+	end
+end)
